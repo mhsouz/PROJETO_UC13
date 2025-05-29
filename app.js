e98ff9d8ed5c4d8591a33543b670f130cd39ddeb
marcos souza <marcos.hsouza@senacsp.edu.br>
2025-05-28 22:08:41 -0300

    minha primeira rota em node.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world!!!");
+//console.log("hello world!!!");
+
+const express = require ('express'); // express = framework para facilitar criação web
+
+const app = express(); //app é o servidor web
+
+app.get ("/", function(req,res){     // efetuando requisição tipo get ,  / = na url , req = requisição , res= resposta
+    res.write("hello word !!");
+    res.end();                          // finalizar resposta
+});
+
+app. listen(8080); // 
